b6f1e91f0fdb49db4614042190e008cebc9391b4
Alex Lockhart <salexlockhart@gmail.com>
2016-03-05 11:53:00 -0400

    CSS and such

--- www/js/app.js
+++ www/js/app.js
@@ -7,67 +7,69 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/multireddits',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/multireddits.html'
-      }
-    }
-  })
-
-  .state('app.subreddits', {
-      url: '/subreddits',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/subreddits.html'
-        }
-      }
-    })
-    .state('app.posts', {
-      url: '/posts',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/posts.html',
-          controller: 'PostsCtrl'
-        }
-      }
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
     })
 
-  .state('app.post', {
-    url: '/posts/:postId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/post.html',
-        controller: 'PostCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/posts');
+    .state('app.multireddits', {
+        url: '/multireddits',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/multireddits.html'
+            }
+        }
+    })
+
+    .state('app.subreddits', {
+        url: '/subreddits',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/subreddits.html',
+                controller: 'SubredditCtrl'
+            }
+        }
+    })
+
+    .state('app.listing', {
+        url: '/r/:subreddit/:filter',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/listing.html',
+                controller: 'ListingCtrl'
+            }
+        }
+    })
+
+    .state('app.comments', {
+        url: '/r/:subreddit/comments/:id/:title',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/comments.html',
+                controller: 'CommentsCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/r/frontpage/hot');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,23 @@
+var reddit = new Snoocore({
+    userAgent: 'ReadIt@0.1.0',
+    throttle: 300,
+    oauth: {
+        type: 'implicit',
+        key: '_sty7RuHMXLIfA',
+        redirectUri: 'http://localhost:3000',
+        scope: ['identity', 'read', 'vote']
+    }
+});
+
+function htmlDecode(input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state, $ionicHistory) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -15,58 +32,122 @@ angular.module('starter.controllers', [])
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
         scope: $scope
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.modal = modal;
     });
 
     // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
+    $scope.closeLogin = function() {
         $scope.modal.hide();
     };
 
     // Open the login modal
-    $scope.login = function () {
+    $scope.login = function() {
         $scope.modal.show();
     };
 
     // Perform the login action when the user submits the login form
-    $scope.doLogin = function () {
+    $scope.doLogin = function() {
         console.log('Doing login', $scope.loginData);
 
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
-        $timeout(function () {
+        $timeout(function() {
             $scope.closeLogin();
         }, 1000);
     };
+
+    $scope.menuSelection = function(name) {
+        $ionicHistory.nextViewOptions({
+          disableBack: true
+        });
+        switch (name) {
+            case "frontpage":
+                $scope.goToSubreddit("frontpage");
+                break;
+            case "subredditlist":
+                $state.go("app.subreddits");
+                break;
+            case "multiredditlist":
+                $state.go("app.multireddits");
+                break;
+            default:
+        }
+    };
+
+    $scope.goToComments = function(url) {
+        var ss = url.split("/");
+        $state.go('app.comments', {
+            subreddit: ss[2],
+            id: ss[4],
+            title: ss[5]
+        });
+    };
+
+    $scope.goToSubreddit = function(name) {
+        $state.go('app.listing', {
+            subreddit: name,
+            filter: 'hot'
+        });
+    };
 })
 
-.controller('PostsCtrl', function ($scope) {
-    var reddit = new Snoocore({
-        userAgent: '/u/apollo959 ReadIt@0.0.1',
-        throttle: 300,
-        oauth: {
-            type: 'implicit',
-            key: '_sty7RuHMXLIfA',
-            redirectUri: 'http://localhost:3000',
-            scope: ['identity', 'read', 'vote']
-        }
-    });
+.controller('ListingCtrl', function($scope, $stateParams) {
+    var location = "";
+    $scope.title = "";
+    if ($stateParams.subreddit != 'frontpage') {
+        location = '/r/' + $stateParams.subreddit;
+        $scope.title = '/r/' + $stateParams.subreddit;
+    } else {
+        $scope.title = 'Front Page';
+    }
+    location = location + '/' + $stateParams.filter;
 
-    $scope.test = "Test String";
-
-    reddit('/hot').listing().then(function (slice) {
+    reddit(location).listing().then(function(slice) {
         var postsArray = [];
-        for (post of slice.children) {
+        for (var post of slice.children) {
             if (post.data.thumbnail === "self" || post.data.thumbnail === "") {
-                delete post.data.thumbnail;
+                // delete post.data.thumbnail;
+                post.data.thumbnail = "/img/ionic.png";
             }
+            post.title = htmlDecode(post.title);
             postsArray.push(post.data);
         }
         $scope.posts = postsArray;
-        console.dir($scope.posts);
         $scope.nextPage = slice.next();
     });
 })
 
-.controller('PostCtrl', function ($scope, $stateParams) {});
+.controller('CommentsCtrl', function($scope, $stateParams) {
+    //If I am a self post, I need to display the text of the post
+    $scope.title = "Comments";
+    var parseComments = function(comments) {
+        var list = document.createElement("ion-list");
+        for (var comment of comments) {
+            var item = document.createElement("div");
+            var p = document.createElement("p");
+            // p.className = "item";
+            p.innerHTML = comment.data.body;
+            item.className = "box";
+            item.appendChild(p);
+            if ("replies" in comment.data && comment.data.replies !== "") {
+                item.appendChild(parseComments(comment.data.replies.data.children));
+            }
+            list.appendChild(item);
+        }
+        return list;
+    };
+    reddit("/r/" + $stateParams.subreddit + "/comments/" + $stateParams.id).get().then(function(slice) {
+        var textpost = slice[0];
+        var comments = slice[1];
+        $scope.textpost = textpost.data.children[0].data;
+        document.getElementById("comments").appendChild(parseComments(comments.data.children));
+    });
+})
+
+.controller('SubredditCtrl', function($scope) {
+    $scope.subreddits = [{
+        "displayName": "AskReddit",
+        "name": "askreddit"
+    }];
+});
